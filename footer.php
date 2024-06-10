b7d53a7f687d150b58e1999e2a5ec0cee94ff3cc
Aziz Maulana Hakim <azizmh.0809@gmail.com>
2024-06-10 11:21:55 +0700

    update footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
-    <!-- ##### Footer Area Start ##### -->
-    <footer class="footer-area bg-img" style="background-image: url(img/bg-img/3.jpg);">
+<!-- ##### Footer Area Start ##### -->
+<footer class="footer-area bg-img" style="background-image: url(img/bg-img/3.jpg);">
         <!-- Main Footer Area -->
         <div class="main-footer-area">
             <div class="container">
                 <div class="row">
 
                     <!-- Single Footer Widget -->
-                    <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="col-12 col-sm-6 col-lg-3">
                         <div class="single-footer-widget">
                             <div class="footer-logo mb-30">
                                 <a href="#"><img src="img/core-img/logopagiceria.png" alt="" width="150px" style="margin-left: 40px;"></a>
@@ -22,38 +22,39 @@
                     </div>
 
                     <!-- Single Footer Widget -->
-                    <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="col-12 col-sm-6 col-lg-3">
                         <div class="single-footer-widget">
                             <div class="widget-title">
                                 <h5>QUICK LINK</h5>
                             </div>
                             <nav class="widget-nav">
                                 <ul>
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Contact</a></li>
-                                    <li><a href="#">Service</a></li>
-                                    <li><a href="#">Product</a></li>
+                                    <li><a href="#">Tentang Kami</a></li>
+                                    <li><a href="#">FAQs</a></li>
+                                    <li><a href="#">Pembayaran</a></li>
+                                    <li><a href="#">Produk</a></li>
                                     <li><a href="#">Portofolio</a></li>
+                                    <li><a href="#">Pemesanan</a></li>
                                 </ul>
                             </nav>
                         </div>
                     </div>
 
                     <!-- Single Footer Widget -->
-                    <!-- <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="col-12 col-sm-6 col-lg-3">
                         <div class="single-footer-widget">
                             <div class="widget-title">
                                 <h5>TEMUI KAMI</h5>
-                            </div> -->
+                            </div>
 
                             <!-- Single Best Seller Products -->
                             
-                            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.6220465263045!2d106.83430687430024!3d-6.313279861780123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec307ee6c245%3A0x636bb1e9b24d205c!2sPagi%20Ceria%20Landscaping!5e0!3m2!1sid!2sid!4v1717647423801!5m2!1sid!2sid" width="200px"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" ></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.6220465263045!2d106.83430687430024!3d-6.313279861780123!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec307ee6c245%3A0x636bb1e9b24d205c!2sPagi%20Ceria%20Landscaping!5e0!3m2!1sid!2sid!4v1717647423801!5m2!1sid!2sid" width="200px"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" ></iframe>
                         </div>
-                    </div> -->
+                    </div>
 
                     <!-- Single Footer Widget -->
-                    <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="col-12 col-sm-6 col-lg-3">
                         <div class="single-footer-widget">
                             <div class="widget-title">
                                 <h5>CONTACT</h5>
@@ -83,7 +84,7 @@
                     <div class="col-12 col-md-6">
                         <div class="copywrite-text">
                             <p>&copy; <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | <a href="" target="_blank">Pagi Ceria</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 </p>
                         </div>
@@ -96,8 +97,8 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
                                     <li><a href="#">Home</a></li>
                                     <li><a href="#">About</a></li>
                                     <li><a href="#">Service</a></li>
-                                    <li><a href="#">Product</a></li>
                                     <li><a href="#">Portfolio</a></li>
+                                    <li><a href="#">Blog</a></li>
                                     <li><a href="#">Contact</a></li>
                                 </ul>
                             </nav>
